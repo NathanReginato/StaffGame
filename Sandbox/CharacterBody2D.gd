3f2dcbacd9290be1d91b1b2be88a991191b7fe80
taylorjordan <taylor@campminder.com>
2024-07-30 13:03:16 -0500

    movement updates

--- Sandbox/CharacterBody2D.gd
+++ Sandbox/CharacterBody2D.gd
@@ -1,32 +1,32 @@
 extends CharacterBody2D
-@onready var _animated_sprite = $AnimatedSprite2D
-signal movement(direction)
+@export var speed = 400
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+#
 func _process(_delta):
 	if Input.is_action_pressed("ui_up") && Input.is_action_pressed("ui_right"):
-		movement.emit(2)
 		_animated_sprite.play("WalkNorthEast")
 	elif Input.is_action_pressed("ui_up") && Input.is_action_pressed("ui_left"):
-		movement.emit(8)
 		_animated_sprite.play("WalkNorthWest")
 	elif Input.is_action_pressed("ui_down") && Input.is_action_pressed("ui_right"):
-		movement.emit(4)
 		_animated_sprite.play("WalkSouthEast")
 	elif Input.is_action_pressed("ui_down") && Input.is_action_pressed("ui_left"):
-		movement.emit(6)
 		_animated_sprite.play("WalkSouthWest")
 	elif Input.is_action_pressed("ui_right"):
-		movement.emit(3)
 		_animated_sprite.play("WalkEast")
 	elif Input.is_action_pressed("ui_left"):
-		movement.emit(7)
 		_animated_sprite.play("WalkWest")
 	elif Input.is_action_pressed("ui_up"):
-		movement.emit(1)
 		_animated_sprite.play("WalkNorth")
 	elif Input.is_action_pressed("ui_down"):
-		movement.emit(5)
 		_animated_sprite.play("WalkSouth")
 	else:
-		movement.emit(0)
 		_animated_sprite.stop()
--- Sandbox/Node2D.gd
+++ Sandbox/Node2D.gd
@@ -2,37 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CharacterBody2D.connect("movement",func(direction):
-		match direction: 
-			1: 
-				#Up
-				$TileMap.move_local_y(1)
-			2:
-				#NorthEast
-				$TileMap.move_local_x(-1)
-				$TileMap.move_local_y(1)
-			3:
-				#Right
-				$TileMap.move_local_x(-1)
-			4:
-				#SouthEast
-				$TileMap.move_local_x(-1)
-				$TileMap.move_local_y(-1)
-			5:
-				#Down
-				$TileMap.move_local_y(-1)
-			6:
-				#SouthWest
-				$TileMap.move_local_x(1)
-				$TileMap.move_local_y(-1)
-			7:
-				#Left
-				$TileMap.move_local_x(1)
-			8:
-				#NorthWest
-				$TileMap.move_local_x(1)
-				$TileMap.move_local_y(1)
-	) # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
