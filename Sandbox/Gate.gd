9b3c858418bde46098d58af735381169ad1dd727
Nate <nathan.reginato@gmail.com>
2024-07-30 17:20:43 -0600

    added some more stuff

---
+++ Sandbox/Gate.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.monitoring = true
+
+
+func _on_body_entered(body):
+	print("player entered")
+	get_node("Gate/AnimatedSprite2D").play("gate_open")
