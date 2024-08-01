f3168bdebf892a2772d013d20df5d552c8d16812
taylorjordan <taylor@campminder.com>
2024-08-01 14:56:54 -0500

    treant text

--- Sandbox/Clipboard.gd
+++ Sandbox/Clipboard.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 
+@onready var icecream1 = $"1"
+@onready var icecream2 = $"2"
+@onready var icecream3 = $"3"
+@onready var icecream4 = $"4"
+@onready var icecream5 = $"5"
+
+@onready var progress = $"../WaterBottle/WaterProgress"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +16,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_form_completed_button_pressed():
+	
+	if progress.value <= progress.max_value and icecream1.visible == false:
+		icecream1.visible = true
+	elif progress.value <= progress.max_value and icecream2.visible == false:
+		icecream2.visible = true
+	elif progress.value <= progress.max_value and icecream3.visible == false:
+		icecream3.visible = true
+	elif progress.value <= progress.max_value and icecream4.visible == false:
+		icecream4.visible = true
+	elif progress.value <= progress.max_value and icecream5.visible == false:
+		icecream5.visible = true
+		
+		
