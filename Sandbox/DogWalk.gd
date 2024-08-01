a416900fb78fa471beadbce97635bec63c1db70a
Nate <nathan.reginato@gmail.com>
2024-08-01 10:42:11 -0600

    fixed dog dialog -- mostly

--- Sandbox/DogWalk.gd
+++ Sandbox/DogWalk.gd
@@ -2,19 +2,26 @@ extends Path2D
 
 @onready var path_to_follow = $PathFollow2D
 @onready var javi_sprite = $"PathFollow2D/Doggy Javi/AnimatedSprite2D"
+signal all_done_walking
 
 func _on_sprite_2d_time_for_a_walk():
-	print("time for a walk")
 	javi_sprite.stop()
-	while path_to_follow.progress_ratio < 1:
-		path_to_follow.progress_ratio += 0.001
+	while path_to_follow.progress_ratio < 0.17:
+		# path_to_follow.progress_ratio += 0.002
+		path_to_follow.progress_ratio += 0.05
 		await get_tree().create_timer(0.01).timeout
 		if !javi_sprite.is_playing():
 			javi_sprite.play("walk_down")
-			#
-		#if (path_to_follow.progress_ratio > 0.20) or javi_sprite.is_playing():
-			#javi_sprite.stop()
-			#javi_sprite.play("walk_right")
-
-		
+	
+	javi_sprite.stop()
+	while path_to_follow.progress_ratio >= 0.17 and path_to_follow.progress_ratio < 1:
+		path_to_follow.progress_ratio += 0.05
+		await get_tree().create_timer(0.01).timeout
+		if !javi_sprite.is_playing():
+			javi_sprite.play("walk_right")
 			
+	javi_sprite.stop()
+	javi_sprite.play("idle_left")
+	all_done_walking.emit()
+			
+	
--- Sandbox/DoggyDialog.gd
+++ Sandbox/DoggyDialog.gd
@@ -1,41 +1,63 @@
 extends Sprite2D
 # All of the text to scroll
+@onready var label = $DoggyText
+signal time_for_a_walk
+
 var dialog_list = [
-	"Hello! You must\nbe Dan",
-	"Welcome to Camp!\nWoof! I'm here to\nshow you around",
-	"Let me show you\nwhere we begin"
+	[
+		"Hello! You must\nbe Dan",
+		"Welcome to Camp!\nWoof! I'm here to\nshow you around",
+		"Let me show you\nwhere we begin"
+	],
+	[
+		"Oh! This gate appears to be locked!",
+		"Where's your ice cream sandwich key? ... You don't have any??",
+		"There's only one way at camp to get ice cream sandwiches and it's...",
+		"FILLING OUT FORMS!!!",
+		"Have you filled out any of forms yet? Let's check your status bar.",
+		"Crikey, your clipboard is empty! You haven't filled out any forms. Please go to your campanion app and come back when you've got one done."
+	]
 ]
 
-signal time_for_a_walk
-# Number of characters to display
-var text_shown_index = 0
 var dialog_index = 0
-@onready var label = $DoggyText
-var shown = false
-var showing = false
+var dialog_tree_index = 0
+var willing_to_engage_in_speaking = true
+var currently_speaking = false
+
+func _input(event):
+	if event.is_action_pressed("ui_select") and currently_speaking:
+		var d = dialog_list[dialog_tree_index][dialog_index]
+		var d2 = dialog_list[dialog_tree_index]
+		print("dialog index ", dialog_index, " ", len(d2))
+		await set_ticker_text(d)
+		await get_tree().create_timer(0.1).timeout
+		if dialog_index == len(d2) - 1:
+			currently_speaking = false
+			visible = false
+			willing_to_engage_in_speaking = false
+			dialog_index = 0
+			emit_signal("time_for_a_walk")
+		
+		if dialog_index < len(d2):
+			dialog_index += 1
+		
 
 func _on_area_2d_body_entered(body):
-	if "is_player" in body and body.is_player == true and !showing and !shown:
+	if "is_player" in body and body.is_player == true and willing_to_engage_in_speaking and !currently_speaking:
 		visible = true
-		showing = true
-		await set_ticker_text()	
-		visible = false
-		emit_signal("time_for_a_walk")
-	# Move to place
+		currently_speaking = true
+		await set_ticker_text(dialog_list[dialog_tree_index][dialog_index])
+		dialog_index += 1
 
-func set_ticker_text():
-#	Set the text to the current position + some number of characters
-	for dialog in dialog_list:
+func set_ticker_text(text):
+	label.text = ""
+	for char in text:
+		# Append the character to the variable
+		label.text += char
+		await get_tree().create_timer(0.0007).timeout
 
-		# Initialize an empty variable to store characters
-		var temp_variable = ""
-		# Iterate through each character in the string
-		for char in dialog:
-			# Append the character to the variable
-			temp_variable += char
-		
-			label.text = temp_variable
-			await get_tree().create_timer(0.0007).timeout
-	
-	await get_tree().create_timer(1).timeout
-	shown = true
+func _on_path_2d_all_done_walking():
+	dialog_index = 0
+	dialog_tree_index += 1
+	willing_to_engage_in_speaking = true
+
